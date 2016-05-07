3b598c2afa4b543aa595052702084be342426b88
Vladimir <vovaloik@mail.ru>
2016-05-07 03:09:30 +0300

    123

--- AlexBot/AlexBot.py
+++ AlexBot/AlexBot.py
@@ -14,7 +14,7 @@ def auth_vk(id, login, passwd, scope):
 def send_mesg(who, text):
     attach=re.findall('{\w*\S*\w*}', text)
     if(len(attach) != 0):
-        bot.messages.send(peer_id=who, message=re.sub('{\w*\S*\w*}', "", text), random_id=random.randint(0, 200000), attachment=attach[0][1:-1])
+        bot.messages.send(peer_id=who, message=re.sub('{\w*\S*\w*}', "", text), random_id=random.randint(0, 200000), attachment=attach[1][1:-1])
     else:
         bot.messages.send(peer_id=who, message=text, random_id=random.randint(0, 200000))
 
@@ -129,6 +129,54 @@ def find_hw(who, text):
         return
     bot.messages.send(peer_id=who, message="Неправильный формат!", random_id=random.randint(0, 200000))
 
+def generate_post(who):
+    bot.messages.send(peer_id=who, message="Подгатавливаю пост, ждите(это может быть быстро, а может долго. Производительность рандомная, также как и генерируемый пост...)", random_id=random.randint(0, 200000))
+    p = requests.get("http://loremflickr.com/900/600/all")
+    out = open("random.jpg", "wb")
+    out.write(p.content)
+    out.close()
+
+    img = {'photo': ('img.jpg', open(r'random.jpg', 'rb'))}
+    # Получаем ссылку для загрузки изображений
+    response = bot.photos.getMessagesUploadServer()
+    upload_url = response['upload_url']
+
+    # Загружаем изображение на url
+    response = requests.post(upload_url, files=img)
+    result=response.json()
+
+    # Сохраняем фото на сервере и получаем id
+    photo1=result['photo'] 
+    hash1=result['hash'] 
+    server1=result['server']
+    
+
+    count=0
+
+    while(count == 0):
+        try:
+            ownerid=random.randint(5000, 363720000)
+            count=bot.audio.getCount(owner_id=ownerid)
+            time.sleep(0.1)
+        except 6:
+                    print("Запросы")
+                    time.sleep(0.8)
+                    continue
+
+        
+
+    list_audio=bot.audio.get(owner_id=ownerid)
+    num=random.randint(0, count)
+    audioid=list_audio["items"]
+    audioid=audioid[num]
+    audioid=audioid["id"]
+    if(photo1 != '[]'):
+        response = bot.photos.saveMessagesPhoto(server=server1, photo=photo1,   hash=hash1) 
+        attach="photo"+str(response[0]['owner_id'])+"_"+str(response[0]['id'])+",audio"+str(ownerid)+"_"+str(audioid)
+        bot.messages.send(peer_id=who, random_id=random.randint(0, 200000), attachment=attach)
+
+    
+
     
 
 def main():
@@ -140,42 +188,85 @@ def main():
     home_work_base=[]
     home_work_base=init_home_work_db()
     simple_command=init_simple_command()
-    bot = auth_vk('5419077', '89851906212', 'dicks228', 'wall,messages,photos')
+    bot = auth_vk('5419077', "89851906212", "dicks228", 'wall,messages,photos,audio')
     print("Ready!")
     wf_client = wolframalpha.Client("KW45EP-XHU7PVPVTX")
+    error_message="Упс! Что то пошло не так... Попробуйте повторить запрос. Если эта ошибка происходит постоянно, пожалуйста, свяжитесь с vk.com/id96494615 для устранения проблеммы"
     while (True):
         try:
             poll = bot.messages.getLongPollServer()
             r = requests.request("GET","http://"+poll['server']+"?act=a_check&key="+poll['key']+"&ts="+str(poll['ts'])+"&wait=25&mode=2", timeout = 50)
+            mesg_poll=r.json()
         except Exception:
             print("Error")
             time.sleep(4)
             poll = bot.messages.getLongPollServer()
             continue
-
-        for mesg in r.json()['updates']:
+        for mesg in mesg_poll['updates']:
             if (mesg[0] != 4):
                 continue
             if (mesg[6].split(" ")[0] == "/список"):
-                send_list(mesg[3])
-                continue           
+                try:
+                    send_list(mesg[3])
+                    continue    
+                except Exception:
+                   print("Ошибка при отправке списка") 
+                   bot.messages.send(peer_id=mesg[3], message=error_message, random_id=random.randint(0, 200000))
+                   continue     
             if (mesg[6].split(" ")[0] == "/добавить"):
-                add_simple_command(mesg[6].split(" "))
-                continue
+                try:
+                    add_simple_command(mesg[6].split(" "))
+                    continue
+                except Exception:
+                    print("Ошибка при добавлении команды")
+                    bot.messages.send(peer_id=mesg[3], message=error_message, random_id=random.randint(0, 200000))
+                    continue
             if (mesg[6].split(" ")[0] == "/wolfram"):
-                wolfram_send(mesg[3], mesg[6].split(" "))
-                continue
+                try:
+                    wolfram_send(mesg[3], mesg[6].split(" "))
+                    continue
+                except Exception:
+                    print("Очередной пиздец в вольфраме")
+                    bot.messages.send(peer_id=mesg[3], message=error_message, random_id=random.randint(0, 200000))
+                    continue
+                
             if (mesg[6].split(" ")[0] == "/добавить_дз"):
-                add_home_work(mesg[1], mesg[6].split(" "))
-                continue
+                try:
+                    add_home_work(mesg[1], mesg[6].split(" "))
+                    continue
+                except Exception:
+                    print("Ошибка при добавлении дз")
+                    bot.messages.send(peer_id=mesg[3], message=error_message, random_id=random.randint(0, 200000))
+                    continue
             if (mesg[6].split(" ")[0] == "/дз"):
-                find_hw(mesg[3], mesg[6].split(" "))
-                continue
+                try:
+                    find_hw(mesg[3], mesg[6].split(" "))
+                    continue
+                except Exception:
+                    print("Ошибка при выводе дз")
+                    bot.messages.send(peer_id=mesg[3], message=error_message, random_id=random.randint(0, 200000))
+                    continue
+            if (mesg[6].split(" ")[0] == "/пост"):
+                try:
+                    generate_post(mesg[3])
+                    continue
+                except IndexError:
+                    print("Индекс")
+                    bot.messages.send(peer_id=mesg[3], message=error_message, random_id=random.randint(0, 200000))
+                    continue
+                
+            continue
             if (mesg[6] in simple_command):
-                send_mesg(mesg[3], simple_command[mesg[6]])
-                continue
+                try:
+                    send_mesg(mesg[3], simple_command[mesg[6]])
+                    continue
+                except Exception:
+                    print("Ошибка при отправке команды")
+                    bot.messages.send(peer_id=mesg[3], message=error_message, random_id=random.randint(0, 200000))
+                    continue
 
-   
+
+            
 
 if (__name__ == "__main__"):
     main()
